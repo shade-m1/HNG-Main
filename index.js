b08425f12575f5015f783b94580db76a383daa28
shade-m1 <56414145+shade-m1@users.noreply.github.com>
2020-11-14 15:29:56 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+
+const client = new Discord.Client();
+
+const prefix = 'h!'
+
+client.once('ready' , () =>{
+   console.log('you bot is ready'); 
+});
+
+
+
+
+
+client.login('config.TOKEN')
