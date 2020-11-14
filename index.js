b6452dd5954725e0e8b07150070414a9999453b8
shade-m1 <56414145+shade-m1@users.noreply.github.com>
2020-11-14 16:14:44 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 
 const prefix = 'h!'
-
+client.on('message', message => {
+   if (message.content === '+ping') {  
+     message.channel.send(`🏓Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+   }
+ });
 client.once('ready' , () =>{
    console.log('you bot is ready'); 
 });
